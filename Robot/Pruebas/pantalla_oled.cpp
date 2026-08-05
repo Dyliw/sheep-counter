8318bb60a4c10af3b11e24653e73e305cfdf0fca
Deysy <deysygmtz.dm@gmail.com>
2026-08-05 10:55:51 -0600

    Update pantalla_oled.cpp

--- Robot/Pruebas/pantalla_oled.cpp
+++ Robot/Pruebas/pantalla_oled.cpp
@@ -1,1 +1,24 @@
+#include <U8g2lib.h>
+#include <SPI.h>
 
+#define PIN_SCK 12
+#define PIN_MOSI 11
+#define PIN_CS 8
+#define PIN_DC 9
+#define PIN_RES 10
+
+U8G2_SSDD1306_128X64_NONAME_1_4W_HW_SPI
+
+void setup(){
+  serial.begin(115200);
+  u8g2.begin();
+  
+}
+void loop(){
+  u8g2.firstPage();
+  do{
+    u8g2.clearBuffer();
+    u8g2.drawStr(10, 20, 'Hola Mundo');
+  } while(u8g2.nextPage());
+  delay(1000);
+}
