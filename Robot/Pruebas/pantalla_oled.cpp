19be847cfa08eb7e000b37f94bb5409a8f59f1bd
Deysy <deysygmtz.dm@gmail.com>
2026-08-05 22:24:39 -0600

    Update pantalla_oled.cpp

--- Robot/Pruebas/pantalla_oled.cpp
+++ Robot/Pruebas/pantalla_oled.cpp
@@ -7,7 +7,6 @@
 #define PIN_CS   8
 #define PIN_DC   9
 #define PIN_RES  10
-
 U8G2_SSDD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, PIN_CS, PIN_DC, PIN_RES);
 void setup() {
   Serial.begin(115200);
